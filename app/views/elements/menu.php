cf911488e7c3a06c9f399fa0f35dfecc09fe3881
Mariusz Marciniak <mariusz.marciniak@tuxedomoney.com>
2021-07-27 20:31:55 +0100

    menu

--- app/views/elements/menu.php
+++ app/views/elements/menu.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white">
   <div class="container">
     <a class="navbar-brand" href="#">beachroad</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
+    <!-- <span class="navbar-toggler-icon"></span> -->
     </button>
         <ul class="nav justify-content-end">
             <li class="nav-item">
