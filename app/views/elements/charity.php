c73eeef62fe0c6d2703f2a214bc20b09fc998439
waghtot <waghtot@gmail.com>
2021-08-30 19:25:22 +0100

    update

--- app/views/elements/charity.php
+++ app/views/elements/charity.php
@@ -3,7 +3,7 @@
     <div class="card-body">
       <h5 class="card-title">Charity</h5>
       <p class="card-text">
-        WE ARE PROUD TO SUPPORT Leading Healthcare Charities <a href="https://bridge2aid.org/" target="_blank">Bridge2Aid.ORG</a> & <a href="http://famd.org.uk/" target="_blank">FAMD.ORG.UK</a><br>
+        WE ARE PROUD TO SUPPORT Leading Healthcare Charities <a href="https://bridge2aid.org/" target="_blank" class="link-color">Bridge2Aid.ORG</a> & <a href="http://famd.org.uk/" target="_blank" class="link-color">FAMD.ORG.UK</a><br>
         A significant proportion of tickets sale will be donated to these charities to support their cause during these devastating Coronovairus pandemic.</p>
     </div>
 </div>
--- app/views/elements/tickets.php
+++ app/views/elements/tickets.php
@@ -2,7 +2,7 @@
     <img src="app/views/img/cf_logo.png" alt="FAMD" class="img-thumbnail">
     <div class="card-body">
       <h5 class="card-title">Tickets</h5>
-      <p class="card-text">Please buy your tickets from our partner <a href="https://crowdfunder.co.uk" target="_blank">crowdfunder.co.uk</a><br><br><br><br><br><br></p>
+      <p class="card-text">Please buy your tickets from our partner <a href="https://crowdfunder.co.uk" target="_blank" class="link-color">crowdfunder.co.uk</a><br><br><br><br><br><br></p>
       <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
     </div>
 </div>
